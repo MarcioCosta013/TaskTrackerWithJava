060206ed79178e27ad3c6bd1899bba050c266ed0
Marcio Costa <marcio-costa38@hotmail.com>
2024-12-19 00:23:15 -0300

    create: estrutura CLI basica para lidar com entradas do usuario.

--- src/TaskTracker.java
+++ src/TaskTracker.java
@@ -1,6 +1,47 @@
 public class TaskTracker {
-    
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String command = args[0];
+        switch (command) {
+            case "add":
+                System.out.println("add");
+                break;
+            case "update":
+                System.out.println("update2");
+                break;
+
+            case "delete":
+                System.out.println("delete");
+                break;
+
+            case "mark-in-progress":
+                System.out.println("mark-in-progress");
+                break;
+
+            case "mark-done":
+                System.out.println("mark-done");
+                break;
+
+            case "list":
+                System.out.println("list");
+                break;
+
+            case "list done":
+                System.out.println("list done");
+                break;
+
+            case "list todo":
+                System.out.println("list todo");
+                break;
+
+            case "list in-progress":
+                System.out.println("list in-progress");
+                break;
+
+            default:
+                System.out.println("comando invalido, tente novamente");
+                break;
+        }
     }
 }
